784b83f72ed92da8303281c793afe161cfb37f67
Vipin Kiroula <vipinkiroula@gmail.com>
2024-02-26 21:24:51 +0530

    Fix env issue and add vercel.json

--- api/index.js
+++ api/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const connectWithDB = require('./config/db');
+const dotenv = require('dotenv');
 
 const app = express();
-require('dotenv').config();
+
+dotenv.config();
 
 const PORT = process.env.PORT;
 
