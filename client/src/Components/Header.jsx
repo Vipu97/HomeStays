c44d42b58bd5b12cfb5ae89af8ee7d5e434bd783
Vipin Kiroula <vipinkiroula@gmail.com>
2024-07-09 16:45:17 +0530

    removed unused import

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { UserContext } from "../Context/userContext";
-import PriceFilter from "./PriceFilter";
 
 const Header = ({ searchInput, handleQueryChange }) => {
   const { user } = useContext(UserContext);
